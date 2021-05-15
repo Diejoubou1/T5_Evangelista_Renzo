e9ee183129fe7dd681ea97c4538fff78ed13a2c1
unknown <Diejoubou>
2021-05-15 00:04:34 -0500

    '/c/Users/w10/Desktop/Trabajos TLS/Taller Felipe/T5_Evangelista_Renzo/Assets/Scripts/Electrodomesticos/Renzo Evangelista'#	new file:   Assets/Scripts/Electrodomesticos/Renzo Evangelista/obj/Debug/EvangelistaRenzo.csprojAssemblyReference.cache

---
+++ Assets/Scripts/Electrodomesticos/Renzo Evangelista/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiendaElectrodomesticos;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Clases()
+        {
+            Console.WriteLine("Selecione la Seccion");
+            Console.WriteLine("1 - Estetica");
+            Console.WriteLine("2 - HigienePersonal");
+
+
+        }
+        static void Main(string[] args)
+        {
+            bool nextLoop = true;
+
+            while (nextLoop)
+            {
+                Console.Clear();
+                Loop();
+
+                Console.WriteLine("Continue? (Y/N)");
+                string next = Console.ReadLine();
+
+                if (next.ToUpper() != "Y")
+                    nextLoop = false;
+
+            }
+
+        }
+        static UsoPersonal EsteticaObjects()
+        {
+            UsoPersonal Elementos = null;
+            Console.WriteLine("Selecione la Objetos");
+            Console.WriteLine("1 - AlisadoraDePelo");
+            Console.WriteLine("2 - RizadoraDePelo");
+            string str = Console.ReadLine();
+
+            /*switch (str)
+            {
+                case "1":
+                    Elementos = new AlisadoraDePelo(name);
+                    break;
+
+                case "2":
+                    Elementos = new RizadoraDePelo(name);
+                    break;
+
+                default:
+                    Console.WriteLine("Objeto no encontrado");
+                    break;
+            }
+            */
+
+            return Elementos;
+        }
+        static UsoPersonal HigienePersonalObjects()
+        {
+            UsoPersonal Elementos = null;
+            Console.WriteLine("Selecione la Objetos");
+            Console.WriteLine("1 - Depilora");
+            Console.WriteLine("2 - Afeitadora");
+            Console.WriteLine("3 - CepilloElectrico");
+
+            string str = Console.ReadLine();
+
+            /*switch (str)
+            {
+                case "1":
+                    Elementos = new Afeitadora(name);
+                    break;
+
+                case "2":
+                    Elementos = new Depiladora(name);
+                    break;
+                case "3":
+                    Elementos = new CepilloElectrico(name);
+                    break;
+                default:
+                    Console.WriteLine("Objeto no encontrado");
+                    break;
+            }*/
+
+            return Elementos;
+        }
+
+        static void Loop()
+        {
+            Console.WriteLine("Enter para empezar a editar el precio");
+            Console.ReadLine();
+            UsoPersonal secciones = null;
+            Clases();
+            string STR = Console.ReadLine();
+            /*switch (STR)
+            {
+                case "1":
+                    secciones = EsteticaObjects();
+                    break;
+
+                case "2":
+                    secciones = HigienePersonalObjects();
+                    break;
+                default:
+                    Console.WriteLine("Objeto no encontrado");
+                    break;
+            }*/
+        }
+
+
+    }
+
+}
---
+++ Assets/Scripts/Electrodomesticos/Renzo Evangelista/uso personal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace TiendaElectrodomesticos
+{
+    class UsoPersonal : MonoBehaviour
+    {
+        public string nombre;
+        public string modelo;
+        public string marca;
+        public double precio;
+        public void cambiarprecio()
+        {
+            Console.WriteLine("Cambiar Precio (Precio Actual:" +  precio + ")");
+
+            double nuevoprecio = Convert.ToDouble(Console.ReadLine());
+            precio = nuevoprecio;
+            Console.WriteLine("Nuevo precio: " + precio);
+        }
+
+
+        public UsoPersonal()
+        {
+
+        }
+
+    }
+}
